ee66fa79ddaafbf51fa5d05e4893e12bfbfe86aa
Pablov <juan.vasquez@valposystem.com>
2024-03-15 01:16:21 -0300

    Agregar constructor con parámetros para clase Cliente.java

--- banco/src/banco/Cliente.java
+++ banco/src/banco/Cliente.java
@@ -13,4 +13,15 @@ public class Cliente {
     private String nombre;
     private Cuenta cuenta;
 
+    /**
+     *
+     * @param ID
+     * @param nombre
+     * @param cuenta
+     */
+    public Cliente(Integer ID, String nombre, Cuenta cuenta) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+    }
 }
