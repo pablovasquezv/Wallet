a11bc98228b3346f83c3f06c7bcbe3e7bda7a0ae
Pablov <juan.vasquez@valposystem.com>
2024-03-15 00:18:23 -0300

    Creacion de constructor con parámetros para la clase Cuenta.java

--- banco/src/banco/Cuenta.java
+++ banco/src/banco/Cuenta.java
@@ -7,4 +7,18 @@ public class Cuenta {
     Integer numero_cuenta;
     String titular;
     Double saldo;
+
+    /**
+     *
+     * @param numero_cuenta
+     * @param titular
+     * @param saldo
+     */
+    public Cuenta(Integer numero_cuenta, String titular, Double saldo) {
+        this.numero_cuenta = numero_cuenta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    +
 }
