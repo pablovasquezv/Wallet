4ab582c1708cdd96107eb78ce5cff28cbc5352cc
Pablov <juan.vasquez@valposystem.com>
2024-03-15 00:58:52 -0300

    Agregar los  métodos getters y setters correspondientes clase Cuenta.java. .

--- banco/src/banco/Cuenta.java
+++ banco/src/banco/Cuenta.java
@@ -8,7 +8,7 @@ package banco;
  * Clase: Cuenta
  */
 public class Cuenta {
-    private   Integer numero_cuenta;
+    private Integer numero_cuenta;
     private String titular;
     private Double saldo;
 
@@ -20,6 +20,48 @@ public class Cuenta {
     public Cuenta(Integer numero_cuenta, String titular, Double saldo) {
         this.numero_cuenta = numero_cuenta;
         this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return numero_cuenta
+     */
+    public Integer getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    /**
+     * @param numero_cuenta
+     */
+    public void setNumero_cuenta(Integer numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    /**
+     * @return titular
+     */
+    public String getTitular() {
+        return titular;
+    }
+
+    /**
+     * @param titular
+     */
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    /**
+     * @return saldo
+     */
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo
+     */
+    public void setSaldo(Double saldo) {
         this.saldo = saldo;
     }
 
