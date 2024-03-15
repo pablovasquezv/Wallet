e5cc49c0f271d6b6444d5f9cebc8c25e0ef18995
Pablov <juan.vasquez@valposystem.com>
2024-03-15 00:29:40 -0300

    Crear un método para mostrar los valores de los atributos por pantalla en clase Cuenta.java .

--- banco/src/banco/Cuenta.java
+++ banco/src/banco/Cuenta.java
@@ -9,7 +9,6 @@ public class Cuenta {
     Double saldo;
 
     /**
-     *
      * @param numero_cuenta
      * @param titular
      * @param saldo
@@ -20,5 +19,12 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
-    +
+    /**
+     * Método para mostrar los valores de los atributos por pantalla
+     */
+    public void mostrarInformación() {
+        System.out.println("Número de cuenta: " + numero_cuenta);
+        System.out.println("Titular: " + titular);
+        System.out.println("Saldo: " + saldo);
+    }
 }
