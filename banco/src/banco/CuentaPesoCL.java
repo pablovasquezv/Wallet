6e21a74e59381facba439df5486c8b551c466c75
Pablov <juan.vasquez@valposystem.com>
2024-03-17 23:12:14 -0300

    Creación de método depositar y retiar en clase CuentaPesoCL.java

--- banco/src/banco/CuentaPesoCL.java
+++ banco/src/banco/CuentaPesoCL.java
@@ -1,4 +1,22 @@
 package banco;
 
-public class CuentaPesoCL {
+public class CuentaPesoCL extends Cuenta {
+
+    /**
+     *
+     * @param monto
+     */
+    @Override
+    public void depositar(double monto) {
+        super.depositar(monto);
+    }
+
+    /**
+     *
+     * @param monto
+     */
+    @Override
+    public void retirar(double monto) {
+        super.retirar(monto);
+    }
 }
