2a23fdf22dae5709ba03eff876eb96e968c084af
Pablov <juan.vasquez@valposystem.com>
2024-03-18 16:04:33 -0300

    Creación de atributos y métodos para IMoneda.java

--- banco/src/banco/interfaces/IMoneda.java
+++ banco/src/banco/interfaces/IMoneda.java
@@ -1,4 +1,17 @@
 package banco.interfaces;
 
+/**
+ * @author Pablo
+ */
+
+import banco.models.Moneda;
+
+/**
+ * Interface: IMoneda
+ */
 public interface IMoneda {
+    //Atributos
+    String getSimbolo();
+    double  getFactorConversion();
+    void convertirMoneda(IMoneda moneda,double monto);
 }
