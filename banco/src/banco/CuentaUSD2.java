ff14e70c2709443fe3555e4fdf6cc012e29d449a
Pablov <juan.vasquez@valposystem.com>
2024-03-18 15:44:49 -0300

    Creación las subclases CuentaUSD2 extendiendo de CuentaDigital con método verificarFondos() en clase CuentaUSD2.java

---
+++ banco/src/banco/CuentaUSD2.java
@@ -0,0 +1,20 @@
+package banco;
+
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: CuentaUSD2
+ */
+public class CuentaUSD2 extends CuentaDigital {
+
+    /**
+     *
+     */
+    @Override
+    public void verificarFondos() {
+        // Lógica para verificar fondos en dólares
+        System.out.println("Verificando fondos en dólares...");
+    }
+}
