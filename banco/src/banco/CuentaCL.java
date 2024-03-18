5ea1b4521e4e31e8309ad387b31d6313c1c0bad9
Pablov <juan.vasquez@valposystem.com>
2024-03-18 15:40:16 -0300

    Creación las subclases CuentaCL extendiendo de CuentaDigital con método verificarFondos() en clase CuentaCL.java

---
+++ banco/src/banco/CuentaCL.java
@@ -0,0 +1,18 @@
+package banco;
+
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: CuentaCL
+ */
+
+public class CuentaCL extends CuentaDigital {
+
+    @Override
+    public void verificarFondos() {
+        // Lógica para verificar fondos en pesos chilenos
+        System.out.println("Verificando fondos en pesos chilenos...");
+    }
+}
