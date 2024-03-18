46acdbbfec24bf655d39985f9fb0fdc768942aa3
Pablov <juan.vasquez@valposystem.com>
2024-03-18 14:03:15 -0300

    Crear sub-clase Moneda.java con el método relizarPago

---
+++ banco/src/banco/Moneda.java
@@ -0,0 +1,16 @@
+package banco;
+
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: Moneda
+ */
+public class Moneda extends FormaDePago{
+
+    @Override
+    public void realizarPago() {
+        super.realizarPago();
+    }
+}
