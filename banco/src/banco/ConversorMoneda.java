c9438aef03f6050d228d4ef1336358c97a302f6a
Pablov <juan.vasquez@valposystem.com>
2024-03-18 15:08:56 -0300

    Creación de métodos para convertir montos entre dólares, yenes y libras esterlinas ConversorMoneda.java

--- banco/src/banco/ConversorMoneda.java
+++ banco/src/banco/ConversorMoneda.java
@@ -8,5 +8,75 @@ package banco;
  * Clase: ConversorMoneda
  */
 public class ConversorMoneda {
+    /**
+     * Métodos para convertir montos de dólares a yenes.
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirDolaresAYenes(double monto) {
+        // Lógica para convertir de dólares a yenes
+        double montoConvertido = monto * 110.99; // Tasa de cambio de dólares a yenes
+        return montoConvertido;
+    }
 
+    /**
+     * Métodos para convertir montos de dólares a libras esterlinas
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirDolaresALibrasEsterlinas(double monto) {
+        // Lógica para convertir de dólares a libras esterlinas
+        double montoConvertido = monto * 0.72; // Tasa de cambio de dólares a libras esterlinas
+        return montoConvertido;
+    }
+
+    /**
+     * Métodos para convertir montos de yenes a dólares.
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirYenesADolares(double monto) {
+        // Lógica para convertir de yenes a dólares
+        double montoConvertido = monto / 110.99; // Tasa de cambio de yenes a dólares
+        return montoConvertido;
+    }
+
+    /**
+     * Métodos para convertir montos de enes a esterlinas.
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirYenesALibrasEsterlinas(double monto) {
+        // Lógica para convertir de yenes a libras esterlinas
+        double montoConvertido = monto * 0.0065; // Tasa de cambio de yenes a libras esterlinas
+        return montoConvertido;
+    }
+
+    /**
+     * Métodos para convertir montos de libras esterlinas a dólares.
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirLibrasEsterlinasADolares(double monto) {
+        // Lógica para convertir de libras esterlinas a dólares
+        double montoConvertido = monto / 0.72; // Tasa de cambio de libras esterlinas a dólares
+        return montoConvertido;
+    }
+
+    /**
+     * Métodos para convertir montos de libras esterlinas yenes.
+     *
+     * @param monto
+     * @return montoConvertido
+     */
+    public double convertirLibrasEsterlinasAYenes(double monto) {
+        // Lógica para convertir de libras esterlinas a yenes
+        double montoConvertido = monto / 0.0065; // Tasa de cambio de libras esterlinas a yenes
+        return montoConvertido;
+    }
 }
