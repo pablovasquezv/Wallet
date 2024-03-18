b14eff7569d9924106bb171cc7ee5a1699bafe74
Pablov <juan.vasquez@valposystem.com>
2024-03-17 23:35:16 -0300

    Creación de lógica de método depositar y retiar en clase CuentaUSD.java.

--- banco/src/banco/CuentaUSD.java
+++ banco/src/banco/CuentaUSD.java
@@ -1,5 +1,12 @@
 package banco;
 
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: CuentaUSD
+ */
 public class CuentaUSD extends Cuenta {
     /**
      *
@@ -7,7 +14,10 @@ public class CuentaUSD extends Cuenta {
      */
     @Override
     public void depositar(double monto) {
-        super.depositar(monto);
+        saldo = 0.0;
+        saldo += monto;//Asignar al saldo el monto
+        System.out.println("Se a depositado " + monto + " de dinero en la cuenta.");
+        System.out.println("El saldo actual es: " + saldo);
     }
 
     /**
@@ -16,6 +26,13 @@ public class CuentaUSD extends Cuenta {
      */
     @Override
     public void retirar(double monto) {
-        super.retirar(monto);
+        /**
+         * En este operador ternario, la condición verifica si el monto es menor o igual al saldo. Si es verdadero,
+         * resta el monto del saldo y muestra un mensaje con el monto retirado y el saldo actual. Si es falso,
+         * muestra un mensaje de fondos insuficientes para realizar el retiro.
+         */
+        saldo = (monto <= saldo) ? (saldo - monto) : saldo;
+        System.out.println((monto <= saldo) ? "Se han retirado " + monto + " de dinero en la cuenta.\n " +
+                "El saldo actual es: " + saldo : "No tiene saldo suficiente para realizar un retiro!");
     }
 }
