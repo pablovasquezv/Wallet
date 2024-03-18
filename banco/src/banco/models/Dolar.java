030b265f742cb1630a55ecbd0dff40370b3b893d
Pablov <juan.vasquez@valposystem.com>
2024-03-18 16:39:20 -0300

    Creación métodos y lógica de convertiMoneda en clase Dolar.java

--- banco/src/banco/models/Dolar.java
+++ banco/src/banco/models/Dolar.java
@@ -11,18 +11,38 @@ import banco.interfaces.IMoneda;
  * Clase: Dolar
  */
 public class Dolar implements IMoneda {
+
+    /**
+     * Método para obtener un simbolo Dólares
+     *
+     * @return USD
+     */
     @Override
     public String getSimbolo() {
-        return null;
+        return "USD";
     }
 
+    /**
+     * Método para obtener Factor Conversion.
+     *
+     * @return
+     */
     @Override
     public double getFactorConversion() {
-        return 0;
+        return 1.0;
     }
 
+    /**
+     * Función para convertir las Monedas a dólares.
+     *
+     * @param otraMoneda
+     * @param monto
+     */
     @Override
-    public void convertirMoneda(IMoneda moneda, double monto) {
-
+    public void convertirMoneda(IMoneda otraMoneda, double monto) {
+        double factorConversion = otraMoneda.getFactorConversion() / this.getFactorConversion();
+        double montoConvertido = monto * factorConversion;
+        System.out.println(monto + " " + this.getSimbolo() + " equivale a: " + montoConvertido + " " +
+                otraMoneda.getSimbolo());
     }
 }
