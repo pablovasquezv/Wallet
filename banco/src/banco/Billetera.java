b8e478cda3eaa012687e14df7cb583afcaf955dc
Pablov <juan.vasquez@valposystem.com>
2024-03-18 14:17:47 -0300

    Crear clase Billetera.java declararemos una referencia de tipo FormaDePago:

---
+++ banco/src/banco/Billetera.java
@@ -0,0 +1,16 @@
+package banco;
+
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: Billetera
+ */
+public class Billetera {
+    FormaDePago metodoPago;
+
+    metodoPago = new TarjetaDeCredito();
+    metodoPago = new Moneda();
+
+}
