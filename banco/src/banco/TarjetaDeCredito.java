40660665fc1bab2e08db1529f097504db203ea84
Pablov <juan.vasquez@valposystem.com>
2024-03-18 14:01:31 -0300

    Crear sub-clase TarjetaDeCredito.java con el método relizarPago

---
+++ banco/src/banco/TarjetaDeCredito.java
@@ -0,0 +1,18 @@
+package banco;
+
+/**
+ * @author Pablo
+ */
+
+/**
+ * Clase: TarjetaDeCredito
+ */
+public class TarjetaDeCredito extends FormaDePago{
+    /**
+     *
+     */
+    @Override
+    public void realizarPago() {
+        super.realizarPago();
+    }
+}
